dec5bd55b10125c8818c45a6cd4412ed912fb5c2
DanielGoycochea <danielgoycocheardz@gmail.com>
2019-08-08 22:42:33 -0500

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="menu">
+        <div className="contenedor">
+          <p className="logo">Daniel Goycochea</p>
+          <nav>
+            <ul>
+              {/* colocar liga  */}
+              <li>Home</li>
+              <li>Quien Soy</li>
+              <li>Trabajos</li>
+              <li>Contacto</li>
+              <li>Otro</li>
+            </ul>
+          </nav>
+        </div>
+
+      </div>
+   
+    </header>
+  );
+};
+
+export default Header;
