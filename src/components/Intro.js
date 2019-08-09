bf4aca64c87fd225bd91c2f9219d23cb57043dc0
DanielGoycochea <danielgoycocheardz@gmail.com>
2019-08-09 00:03:15 -0500

    intro add

---
+++ src/components/Intro.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <section id= "intro">
+      <div className="contenedor">
+        <div className="contenedor-titulo">
+          <div className="titulo">
+            <h1>Desarrollo Web</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus deleniti voluptas eveniet laborum quo veritatis, dicta excepturi architecto similique libero laboriosam eos recusandae fugiat nobis neque vel repellat consectetur odio!</p>
+            <div className="botones">
+              <button className="view">TRABAJOS</button>
+              <button className="contact">CONTACTO</button>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </section>
+  );
+};
+
+export default Intro;
